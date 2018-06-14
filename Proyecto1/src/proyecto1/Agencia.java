28f30127ab63f0bf0bb74b46c5a77a633bddc925
EXOi7\Tongas <gaston.porte13@gmail.com>
2018-06-14 18:09:31 -0300

    agregar clase agencia

---
+++ Proyecto1/src/proyecto1/Agencia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+import static java.nio.file.Files.list;
+import static java.rmi.Naming.list;
+import static java.util.Collections.list;
+
+
+/**
+ *
+ * @author Tongas
+ */
+public class Agencia {
+    int numeroAgencia;
+    list<Vehiculo> unidades[];
+    
+}
