aa357763358833cd76713839ab068bd4b2933f01
Pablo <Pablo@Pablo-PC.home>
2018-06-20 11:10:49 -0300

    commit pablo

--- Proyecto1/src/proyecto1/Main.java
+++ Proyecto1/src/proyecto1/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("esto es una prueba");
+        System.out.println("segundo");
     }
     
 }
